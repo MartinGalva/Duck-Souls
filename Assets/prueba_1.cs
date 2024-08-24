e8fdd26ea7bd88c057128ddd8df27d0532535981
MartinGalva <13.martingalva@gmail.com>
2024-08-23 22:08:51 -0300

    prueba
    prueba

---
+++ Assets/prueba_1.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prueba_1 : MonoBehaviour {
+    private float speed = 8f;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float JumpForce = 5f;
+
+
+    void Update ()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+
+        rb.transform.Translate(new Vector2(h, 0) * Time.deltaTime * speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+}
