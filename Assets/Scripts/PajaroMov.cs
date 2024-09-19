1053c77882fde332fb75159af7db5dedc5164420
MartinGalvarini <13.martingalva@gmail.com>
2024-09-19 19:17:17 -0300

    Build 1.0
    Primer Build!

--- Assets/Scripts/PajaroMov.cs
+++ Assets/Scripts/PajaroMov.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Tilemaps;
+//using UnityEditor.Tilemaps;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -16,7 +16,6 @@ public class PajaroMov : MonoBehaviour
     
     private float tiempoDisparo;
 
-    
     // Start is called before the first frame update
     void Start()
     {
