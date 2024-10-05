5a65d518d462cceb1d03765b2d9f578b7f99741a
unknown <13.martingalva@gmail.com>
2024-10-04 22:02:14 -0300

    Cosas

--- Assets/Scripts/moneda.cs
+++ Assets/Scripts/moneda.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class moneda : MonoBehaviour
 {
+    public int pointsToAdd;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")) {
             // Destruye la moneda para simular que fue recogida
-            Destroy(gameObject);       
+            Destroy(gameObject);
+            PointSytem.Instance.AddingPoints(pointsToAdd);
         }
     }
+
+    
 }
---
+++ Assets/Scripts/pointSystem.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class PointSytem : MonoBehaviour
+{
+    public static PointSytem Instance;
+    public TextMeshProUGUI textPoints;
+    public GameObject coin1;
+    public GameObject coin2;
+    public GameObject coin3; 
+    public Color color;
+    private int points = 0;
+
+
+    public void AddingPoints(int quantity)
+    {
+        points += quantity;
+        UpdateUI();
+    }
+
+
+    public void SubstractingPoints(int quantity)
+    {
+        points -= quantity;
+        UpdateUI();
+    }
+
+
+    private void UpdateUI()
+    {
+        if (points < 0)
+        {
+            points = 0;
+        }
+
+
+        textPoints.text = "Coins: " + points.ToString();
+
+        if (points == 1)
+        {
+            coin1.GetComponent<Image>();
+        }
+    }
+
+
+    private void Awake()
+    {
+        
+
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
