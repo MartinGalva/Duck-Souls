0e0fdae50ff1dae59ee24c8fb36119c3cd6010fe
JuaniMussino <juaniokmussino@gmail.com>
2024-10-26 20:38:08 -0300

    Patrullaje y Chasing
    el oso patrulla una zona y persigue al patito

--- Assets/Scripts/EnemyPatrolX.cs
+++ Assets/Scripts/EnemyPatrolX.cs
@@ -10,40 +10,105 @@ public class EnemyPatrol : MonoBehaviour
     private Rigidbody2D rb;
     private Transform currentPoint;
     [SerializeField] private float speed;
+    public float limiteIzquierdo;
+    public float limiteDerecho;
+    //public float distanciaIzquierda;
+    //public float distanciaDerecha;
+    private bool seMueve =  true;
+
+    //Chase
+    [SerializeField] private Transform playerTransform;
+    private bool isChasing;
+    [SerializeField] private float chaseDistance;
+
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        currentPoint = pointB.transform;
+        //currentPoint = pointB.transform;
+        //limiteIzquierdo = transform.position.x - distanciaIzquierda;
+        //limiteDerecho = transform.position.x + distanciaDerecha;
+         transform.position = new Vector3(limiteIzquierdo, transform.position.y, transform.position.z);
+
+        /*if(transform.localScale.x > 0) {
+            Flip();
+        }*/
     }
 
     void Update()
     {
-        Vector2 point = currentPoint.position - transform.position;
-        if (currentPoint == pointB.transform)
+        
+
+
+
+        //patrullaje solo anda bien
+        /*if(seMueve) {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        if(transform.position.x >= limiteDerecho) {
+            Flip();
+            seMueve =  false;
+        }
+       } else {
+
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        if(transform.position.x <= limiteIzquierdo) {
+            Flip();
+            seMueve =  true;
+        }
+       }*/
+
+       //Chase
+        if (isChasing)
         {
-            rb.velocity = new Vector2(speed, 0);
+            
+            if (transform.position.x > playerTransform.position.x)
+            {
+                rb.velocity = new Vector2(-speed, rb.velocity.y);  
+            }
+            else if (transform.position.x < playerTransform.position.x)
+            {
+                rb.velocity = new Vector2(speed, rb.velocity.y);  
+            }
         }
         else
         {
-            rb.velocity = new Vector2(-speed, 0);
+            
+            if (seMueve)
+            {
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+                if (transform.position.x >= limiteDerecho)
+                {
+                    Flip();
+                    seMueve = false;  
+                }
+            }
+            else
+            {
+                rb.velocity = new Vector2(-speed, rb.velocity.y);
+                if (transform.position.x <= limiteIzquierdo)
+                {
+                    Flip();
+                    seMueve = true;  
+                }
+            }
+
+            
+            if (Vector2.Distance(transform.position, playerTransform.position) < chaseDistance)
+            {
+                isChasing = true;
+            }
         }
 
 
-        if (Vector2.Distance(rb.transform.position, currentPoint.position) > 0.5f && currentPoint == pointA.transform)
-        {
-            currentPoint = pointB.transform;
-            //Flip();
-        }
-
-
-        if (Vector2.Distance(rb.transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
-        {
-            currentPoint = pointA.transform;
-            //Flip();
-        }
+      
+       
+        
     }
+
+
 
     private void Flip()
     {
@@ -51,6 +116,8 @@ public class EnemyPatrol : MonoBehaviour
         localScale.x *= -1;
         transform.localScale = localScale;
     }
+
+
 
     
 
